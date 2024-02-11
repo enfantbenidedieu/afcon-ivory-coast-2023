813b074574aec51a7fb86b5c521ce58bc67f9e62
Duverier <duverierdjifack@gmail.com>
2024-02-11 19:59:40 +0100

    change data prepartion file

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -3,23 +3,23 @@
 import pandas as pd
 
 ##### Import all sheet
-files_xlsx = pd.ExcelFile("./data/AFCON-2023.xlsx")
+#files_xlsx = pd.ExcelFile("data/AFCON-2023.xlsx")
 
 # Team information
-team_infos =  pd.read_excel(files_xlsx, sheet_name="Team Infos")
+team_infos =  pd.read_excel("data/AFCON-2023.xlsx", sheet_name="Team Infos")
 
 # Stadium name 
-Stadium = pd.read_excel(files_xlsx, sheet_name="Stadium")
+Stadium = pd.read_excel("data/AFCON-2023.xlsx", sheet_name="Stadium")
 
 # Overall Data
-data = pd.read_excel(files_xlsx, sheet_name="AFCON-2023")
+data = pd.read_excel("data/AFCON-2023.xlsx", sheet_name="AFCON-2023")
 
 # Goals Data
-goals = pd.read_excel(files_xlsx, sheet_name="Goals Overall")
+goals = pd.read_excel("data/AFCON-2023.xlsx", sheet_name="Goals Overall")
 
 ### Goals Team
-goals_team =  pd.read_excel(files_xlsx, sheet_name="Goals Team")
+goals_team =  pd.read_excel("data/AFCON-2023.xlsx", sheet_name="Goals Team")
 
 ### Data set dictionnary
-data_description = pd.read_excel(files_xlsx, sheet_name="Description")
+data_description = pd.read_excel("data/AFCON-2023.xlsx", sheet_name="Description")
 
